a2cde0bc053bbfe2403d2d4371fa665767ff39ea
Anais <anais.levi20@gmail.com>
2021-08-08 18:43:49 -0400

    first commit

---
+++ src/main/java/com/currencyconverter/App.java
@@ -0,0 +1,18 @@
+package com.currencyconverter;
+import org.apache.http.client.utils.URIBuilder;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        
+    }
+}

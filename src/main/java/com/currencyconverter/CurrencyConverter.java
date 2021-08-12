45b026df5709666cb5935b0ba78aa279822cefdd
Anais <anais.levi20@gmail.com>
2021-08-12 16:34:07 -0400

    Create new class

--- src/main/java/com/currencyconverter/CurrencyConverter.java
+++ src/main/java/com/currencyconverter/CurrencyConverter.java
@@ -1,49 +1,12 @@
 package com.currencyconverter;
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.util.Properties;
 
-import org.apache.http.client.utils.URIBuilder;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import javax.swing.JFrame;
 
-public final class CurrencyConverter {
-    public static void main(String[] args) throws URISyntaxException, IOException, ParseException {
-        Properties prop=new Properties();
-        FileInputStream file= new FileInputStream("config.properties");
-        prop.load(file);
-        URIBuilder exchangeRatesURI = new URIBuilder("https://openexchangerates.org/api/latest.json");
-        exchangeRatesURI.setParameter("app_id",prop.getProperty("API_KEY")); //get API_Key and set as query parameter
-        String readLine = null;
-        URL get = new URL(exchangeRatesURI.build().toString()); //URL for request
-        HttpURLConnection connection = (HttpURLConnection) get.openConnection();
-        connection.setRequestMethod("GET"); //Get request
-        int responseCode = connection.getResponseCode();
-        
-        //get call response 
-        if (responseCode == HttpURLConnection.HTTP_OK) {
-            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            StringBuffer response = new StringBuffer();
-            while ((readLine = in .readLine()) != null) {
-                response.append(readLine);
-            } 
-            in.close();
-            //Using the JSON simple library parse the string into a json object
-            JSONParser parser = new JSONParser();
-            JSONObject responseBody = (JSONObject) parser.parse(response.toString());
+public class CurrencyConverter extends JFrame{
+    public CurrencyConverter(JFrame frame){
 
-            //Get the rate object from the above created JSONObject
-            JSONObject rates = (JSONObject) responseBody.get("rates");
-        
-        } else {
-            throw new RuntimeException("HttpResponseCode: " + responseCode);
-        }
-    
+    }
+    public static void main(String[] args) {
+        CurrencyConverter application = new CurrencyConverter(new JFrame());
     }
 }
---
+++ src/main/java/com/currencyconverter/Exchange.java
@@ -0,0 +1,50 @@
+package com.currencyconverter;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Properties;
+
+import org.apache.http.client.utils.URIBuilder;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+public class Exchange {
+    
+    public static Double getConvertedAmount(String fromCurrency, String toCurrency, Double amount) throws URISyntaxException, IOException, ParseException{
+        Properties prop=new Properties();
+        FileInputStream file= new FileInputStream("config.properties");
+        prop.load(file);
+        URIBuilder exchangeRatesURI = new URIBuilder("https://openexchangerates.org/api/latest.json");
+        exchangeRatesURI.setParameter("app_id",prop.getProperty("API_KEY")); //get API_Key and set as query parameter
+        String readLine = null;
+        URL get = new URL(exchangeRatesURI.build().toString()); //URL for request
+        HttpURLConnection connection = (HttpURLConnection) get.openConnection();
+        connection.setRequestMethod("GET"); //Get request
+        int responseCode = connection.getResponseCode();
+        
+        //get call response 
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuffer response = new StringBuffer();
+            while ((readLine = in .readLine()) != null) {
+                response.append(readLine);
+            } 
+            in.close();
+            //Using the JSON simple library parse the string into a json object
+            JSONParser parser = new JSONParser();
+            JSONObject responseBody = (JSONObject) parser.parse(response.toString());
+
+            //Get the rate object from the above created JSONObject
+            JSONObject rates = (JSONObject) responseBody.get("rates");
+        
+        } else {
+            throw new RuntimeException("HttpResponseCode: " + responseCode);
+        }
+        return 0.0;
+    }
+}
